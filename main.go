98c747007ad7e06843f285025b4dc05435df9ba6
Frank Xu <frank.xu@northbright.com>
2021-08-05 13:12:15 +0800

    Add comments

--- main.go
+++ main.go
@@ -7,25 +7,33 @@ import (
 	"time"
 )
 
+// Task can report progress when doing the work.
 type Task struct {
-	Name     string
+	Name string
+	// Used to store the progress channel.
 	progress atomic.Value
 	mu       sync.Mutex
 	Done     chan struct{}
 }
 
+// NewTask creates the task by given task name.
 func NewTask(name string) *Task {
 	return &Task{Name: name, Done: make(chan struct{})}
 }
 
+// ProgressCh returns a channel to receive the task progress.
 func (t *Task) ProgressCh() <-chan int {
 	ch := t.progress.Load()
 	if ch != nil {
 		return ch.(chan int)
 	}
+
+	// Even load / save atomic.Value is goroutine safe,
+	// still need mutex to protect the "transaction(load and store atomic.Value)" between differents goroutines.
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	ch = t.progress.Load()
+	// Lazily created at first ProgressCh() is called.
 	if ch == nil {
 		ch = make(chan int)
 		t.progress.Store(ch)
@@ -34,10 +42,12 @@ func (t *Task) ProgressCh() <-chan int {
 	return ch.(chan int)
 }
 
+// Run starts the task work.
 func (t *Task) Run() {
 	fmt.Printf("Task: %v is running...\n", t.Name)
 
 	for i := 0; i <= 100; i += 10 {
+		// Send progress data to the channel if the channel exists.
 		ch, _ := t.progress.Load().(chan int)
 		if ch != nil {
 			ch <- i
@@ -49,6 +59,10 @@ func (t *Task) Run() {
 	t.Done <- struct{}{}
 }
 
+// TestA is the most common use case.
+// Starts the task in a new goroutine and create a for-select loop at once.
+// Task's progress channel will be created at the first ProgressCh() is called(immediately after task goroutine started).
+// It can capture the whole progress from 0 - 100.
 func TestA() {
 	t := NewTask("A")
 
@@ -67,6 +81,8 @@ func TestA() {
 	}
 }
 
+// TestB has a delay to create the for-select loop(also the progress channel),
+// so it may lose some progress data(0 - 20) until the ProgressCh() is called.
 func TestB() {
 	t := NewTask("B")
 
@@ -87,6 +103,7 @@ func TestB() {
 	}
 }
 
+// TestC ignores the progress data and the progress channel will not be created at all.
 func TestC() {
 	t := NewTask("C")
 	go func() {
@@ -100,4 +117,33 @@ func main() {
 	TestA()
 	TestB()
 	TestC()
+
+	// Output:
+	//Task: A is running...
+	//Task: A: 0%
+	//Task: A: 10%
+	//Task: A: 20%
+	//Task: A: 30%
+	//Task: A: 40%
+	//Task: A: 50%
+	//Task: A: 60%
+	//Task: A: 70%
+	//Task: A: 80%
+	//Task: A: 90%
+	//Task: A: 100%
+	//Task: A goroutine exits
+	//Task: A: done
+	//Task: B is running...
+	//channel created in ProgressCh()
+	//Task: B: 40%
+	//Task: B: 50%
+	//Task: B: 60%
+	//Task: B: 70%
+	//Task: B: 80%
+	//Task: B: 90%
+	//Task: B: 100%
+	//Task: B goroutine exits
+	//Task: B: done
+	//Task: C is running...
+	//Task: C goroutine exits
 }
